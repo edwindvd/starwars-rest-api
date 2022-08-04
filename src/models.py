1b89a96735268fffb87b9aa211454a3cf9f454a9
edwindvd <edwindavidcr7@gmail.com>
2022-08-04 18:28:54 +0000

    otra vez

--- src/models.py
+++ src/models.py
@@ -4,11 +4,10 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    # favoritos_id = db.Column(db.Integer, db.ForeignKey('favoritos.id'))
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     name = db.Column(String(250), nullable=False)
-    address1 = db.Column(String(250), nullable=False) #si no se usa backref hay que usar backpopulate, investigar esto
+    address1 = db.Column(String(250), nullable=False) 
     favoritos = db.relationship('Favoritos', backref='user') #ver si tengo que agregar el backref='nombredelavariable'
 
     def __repr__(self):
@@ -27,14 +26,14 @@ class User(db.Model):
 class Personajes(db.Model):
     __tablename__ = 'characters'
     id = db.Column(db.Integer, primary_key=True)
-    favoritos_id = db.Column(Integer, db.ForeignKey('favoritos.id'))
+    favoritos = db.relationship('Favoritos', backref='personajes')
     name = db.Column(db.String(250), nullable=False)
     clasificacion = db.Column(db.String(250), nullable=False)
     lenguaje = db.Column(db.String(250), nullable=False)
     def serialize(self):
         return{
             "id": self.id,
-            "favoritos_id": self.favoritos_id,
+            "favoritos": self.favoritos,
             "name": self.name,
             "clasificacion": self.clasificacion,
             "lenguaje": self.lenguaje
@@ -43,13 +42,13 @@ class Personajes(db.Model):
 class Planets(db.Model):
     __tablename__ = 'planetas'
     id = db.Column(db.Integer, primary_key=True)
-    favoritos_id = db.Column(db.Integer, db.ForeignKey('favoritos.id'))
+    favoritos = db.relationship('Favoritos', backref='planets')
     name = db.Column(db.String(250), nullable=False)
     gravity = db.Column(db.String(250), nullable=False)
     def serialize(self):
         return{
             "id": self.id,
-            "favoritos_id": self.favoritos_id,
+            "favoritos": self.favoritos,
             "name": self.name,
             "gravity": self.gravity
         }
@@ -58,10 +57,12 @@ class Favoritos(db.Model):
     __tablename__ = 'favoritos'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    personajes = db.relationship('Personajes', backref='favoritos')
-    planets = db.relationship('Planets', backref='favoritos')
+    personajes_id = db.Column(db.Integer, db.ForeignKey('personajes.id'))
+    planets_id = db.Column(db.Integer, db.ForeignKey('planets.id'))
     def serialize(self):
         return{
             "id": self.id,
             "user_id": self.user_id,
+            "personajes_id": self.personajes_id,
+            "planets_id": self.planets_id
         }
